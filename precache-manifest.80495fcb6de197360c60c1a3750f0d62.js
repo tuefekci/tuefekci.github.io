2e10a17ea5bfaefa06278462ae2ebdcdb895f08b
Giacomo Tüfekci <giacomo@tuefekci.de>
2020-08-25 21:42:08 +0200

    Updates

---
+++ precache-manifest.80495fcb6de197360c60c1a3750f0d62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193249420aa1193bdb213feb410f1c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6abc126807d7a4dd193",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "498598b22a0e51a5ae9d",
+    "url": "/static/js/2.b6ee7eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6ee7eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6abc126807d7a4dd193",
+    "url": "/static/js/main.3316e808.chunk.js"
+  },
+  {
+    "revision": "bc38f714646e8f16f7ca",
+    "url": "/static/js/runtime-main.08a7af36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
